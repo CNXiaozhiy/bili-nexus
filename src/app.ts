2c3b5cffbf62d6277e729a863ed8f02c2b9a43c5
Xiaozhiy <admin@xznet.tech>
2025-12-16 15:25:35 +0800

    fix: spelling mistake
    wtf, 把这个忘了

--- src/app.ts
+++ src/app.ts
@@ -23,14 +23,15 @@ const logger = getLogger("App");
 initVersion();
 
 console.log(`
-    ____     _     __    _     __     _                  __  __            __  
-   / __ )   (_)   / /   (_)   / /    (_) _   __  ___    / / / /  __  __   / /_ 
-  / __  |  / /   / /   / /   / /    / / | | / / / _ \\  / /_/ /  / / / /  / __ \\
- / /_/ /  / /   / /   / /   / /___ / /  | |/ / /  __/ / __  /  / /_/ /  / /_/ /
-/_____/  /_/   /_/   /_/   /_____//_/   |___/  \\___/ /_/ /_/   \\__,_/  /_.___/ 
+    ____     _     __    _     _   __                              
+   / __ )   (_)   / /   (_)   / | / /  ___    _  __  __  __   _____
+  / __  |  / /   / /   / /   /  |/ /  / _ \\  | |/_/ / / / /  / ___/
+ / /_/ /  / /   / /   / /   / /|  /  /  __/ _>  <  / /_/ /  (__  ) 
+/_____/  /_/   /_/   /_/   /_/ |_/   \\___/ /_/|_|  \\__,_/  /____/  
+
 `);
 
-logger.info("BiliLiveHub(BLH) System v" + getVersion());
+logger.info("Bili Nexus v" + getVersion());
 if (process.env.NODE_ENV === "development") logger.debug("当前处于 开发环境");
 else if (process.env.NODE_ENV === "production")
   logger.debug("当前处于 生产环境");
